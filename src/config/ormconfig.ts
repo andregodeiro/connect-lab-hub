2d2be0562868402802ed7315516de1f6a0239cb3
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-14 15:12:55 -0300

    feat: orm config

---
+++ src/config/ormconfig.ts
@@ -0,0 +1,15 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  entities: [],
+  migrations: ['src/migration/**/*.ts'],
+};
+
+export = config;
