da24308d4852b3221182cafce301745ce5430115
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-14 20:41:21 -0300

    api connect with axios

--- front-connect-lab/src/services/api.js
+++ front-connect-lab/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://connectlab.onrender.com/",
+  baseURL: "http://localhost:3000/",
 });
 
 export const createSession = async (email, password) => {
