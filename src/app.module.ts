19ab466395505628eda00a3dd9000634923abfeb
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-14 19:27:46 -0300

    feat: users, devices and authentication modules add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './module/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from './module/authenticate-login.module';
+import 'dotenv/config.js';
+import { DevicesModule } from './module/devices.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+      migrations: ['src/migration/**/*.ts'],
+      migrationsRun: false,
+      logging: true,
+    }),
+    AuthenticationModule,
+    DevicesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
